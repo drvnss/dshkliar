67fa6fe5139a9f5f2c2c809b1613323860b9b406
drvnss <124999786+drvnss@users.noreply.github.com>
2023-02-14 11:33:11 +0300

    практическая 1 14.02

---
+++ main (1).py
@@ -0,0 +1,21 @@
+var_int = 10
+var_float = 8.4
+var_str = "No"
+
+big_int = var_int * 3.5
+
+var_float = var_float - 1.0
+
+ab = var_int / var_float 
+с = big_int / var_float 
+
+var_str = var_str * 2 + "Yes" * 3
+
+print(ab)
+print(var_int)
+print(var_float)
+print(var_str)
+print(big_int)
+print(var_float)
+print(с)
+print(var_str)
